ea14ffb077230ba5dbb8106e8e2ae9578a02aa85
Giovanni Goncalves Vicario <giovanni.goncalves@ibm.com>
2017-12-09 17:25:50 -0200

    Comecando Express

---
+++ app.js
@@ -0,0 +1,14 @@
+// view engine setup
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use ('/',routes);
+app.use('/users',users);
